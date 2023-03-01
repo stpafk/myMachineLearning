7ccfa96057692580608c47df7f0a12f514f7ffb4
stpafk <117909784+stpafk@users.noreply.github.com>
2023-03-01 09:23:48 -0300

    Update linearReg.py

--- forecasting/linearReg.py
+++ forecasting/linearReg.py
@@ -11,6 +11,8 @@ store_sales = pd.read_csv(
     infer_datetime_format=True,
 )
 
+#using time series 
+
 from sklearn.linear_model import LinearRegression
 
 df = average_sales.to_frame()
@@ -26,3 +28,26 @@ model = LinearRegression()
 model.fit(X, y)
 
 y_pred = pd.Series(model.predict(X), index=X.index)
+
+#using lag feature
+
+average_sales = store_sales.groupby('date').mean()['sales']
+
+df = average_sales.to_frame()
+
+lag_1 = df['sales'].shift(1)
+
+df['lag_1'] = lag_1
+
+X = df.loc[:, ['lag_1']]
+X.dropna(inplace=True)  # drop missing values in the feature set
+y = df.loc[:, 'sales']  # create the target
+y, X = y.align(X, join='inner')  # drop corresponding values in target
+
+model = LinearRegression()
+model.fit(X, y)
+
+y_pred = pd.Series(model.predict(X), index=X.index)
+
+
+
