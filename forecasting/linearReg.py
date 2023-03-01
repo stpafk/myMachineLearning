5ffc8a7d68f5f6747bd658fde714967f001bb7f5
stpafk <117909784+stpafk@users.noreply.github.com>
2023-03-01 09:21:31 -0300

    linear regression time series

--- forecasting/linearReg.py
+++ forecasting/linearReg.py
@@ -1,1 +1,28 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
+store_sales = pd.read_csv(
+    comp_dir / 'train.csv',
+    dtype=dtype,
+    parse_dates=['date'],
+    infer_datetime_format=True,
+)
+
+from sklearn.linear_model import LinearRegression
+
+df = average_sales.to_frame()
+
+time = np.arange(len(df.index))  # time dummy
+
+df['time'] = time
+
+X = df.loc[:, ['time']]  # features
+y = df.loc[:, 'sales']  # target
+
+model = LinearRegression()
+model.fit(X, y)
+
+y_pred = pd.Series(model.predict(X), index=X.index)
